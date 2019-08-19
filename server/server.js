fa6937b0171a28d1b1fc807d279e02967296cddb
EP <ep@github.com>
2019-08-19 15:57:15 -0400

    file structure and webpack set up to work in dev environment

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+// app.use(express.static('/client'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/index.html'));
+})
+
+
+app.listen(3000, () => 'Server is listening on port 3000');
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: './index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    hot: true,
+    port: 8085,
+    proxy: [{
+      context: ['/api'],
+      'target': 'http://localhost:3000',
+    }],
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/react', '@babel/preset-env'],
+          compact: false
+        }
+      },
+      {
+        test: /\.scss|css$/,
+        use: [
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(jpeg|jpg|png|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 999999 
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig],
+  stats: {
+    colors: true
+  }
+};
